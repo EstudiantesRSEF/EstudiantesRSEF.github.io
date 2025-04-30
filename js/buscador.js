8e1ac11b94c65cdfdb545aebbfa2bea3b775bdf4
MiguelAlvarezHernanz <124203518+MiguelAlvarezHernanz@users.noreply.github.com>
2025-04-30 17:32:31 +0200

    Update buscador.js

--- js/buscador.js
+++ js/buscador.js
@@ -1,9 +1,11 @@
 function buscarGrupo() {
   const input = document.getElementById("codigo").value.trim().toLowerCase();
+  var groupCard = document.getElementById("group-card");
   const codigo = input.toUpperCase();
   const resultado = document.getElementById("resultado");
   if (codigos[codigo]) {
     resultado.textContent = `You're in the group named: ${codigos[codigo]}`;
+    groupCard.style.display = "block";
   } else {
     resultado.textContent = "Code not found. Please verify that it is correctly written.";
   }
