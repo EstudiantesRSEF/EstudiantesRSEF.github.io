067498cd853ae86e9d21cc60fc77e322261fa37a
MiguelAlvarezHernanz <124203518+MiguelAlvarezHernanz@users.noreply.github.com>
2025-05-01 18:00:36 +0200

    Update buscador_categoria.js

--- js/buscador_categoria.js
+++ js/buscador_categoria.js
@@ -1,42 +1,4 @@
 function buscarCategoria() {
-  const input = document.getElementById("codigo").value.trim().toUpperCase();
-
-  // Get the group card and the elements where we will display the group info
-  var groupCard = document.getElementById("group-card");
-  const lab_group = document.getElementById("lab_group");
-  const cat_group = document.getElementById("cat_group");
-
-  let foundLabGroup = null;
-
-  // Check if the input code exists in the 'codigos' object
-  if (codigos[input]) {
-    // Update the category group info
-    cat_group.textContent = `You're in the gymkhana group: ${codigos[input]}`;
-    groupCard.style.display = "block"; // Show the group card
-  } else {
-    alert("Code not found. Please verify that it is correctly written.");
-  }
-
-  // Check if the input code exists in any of the lab groups
-  for (let group in lab_codes) {
-    if (lab_codes[group].includes(input)) {
-      foundLabGroup = group;
-      break; // Exit the loop once the lab group is found
-    }
-  }
-
-  // If a lab group is found, update the lab group text
-  if (foundLabGroup) {
-    lab_group.textContent = `You're in the lab tour: ${foundLabGroup}`;
-  } else {
-    lab_group.textContent = "No lab tour assigned.";
-  }
-}
-
-
-
-/*
-function buscarCategoria() {
   const input = document.getElementById("codigo").value.trim().toLowerCase();
   const codigo = input.toUpperCase();
   
@@ -63,5 +25,6 @@ function buscarCategoria() {
 
   if (foundLabGroup){
     lab_group.textContent = `You're in the lab tour: ${foundLabGroup}`;
-  }
+  } else {
+    lab_group.textContent = `No lab tour assigned`
 }
