244d8a51e14abb69c85b91256831d3d2a812bcfb
MiguelAlvarezHernanz <124203518+MiguelAlvarezHernanz@users.noreply.github.com>
2025-05-01 18:23:02 +0200

    Update buscador_categoria.js

--- js/buscador_categoria.js
+++ js/buscador_categoria.js
@@ -1,3 +1,33 @@
+function buscarGrupo() {
+  const input = document.getElementById("codigo").value.trim().toLowerCase();
+  var groupCard = document.getElementById("group-card");
+  const codigo = input.toUpperCase();
+  const resultado = document.getElementById("resultado");
+  const labtour = document.getElementById("labtour");
+
+  let foundLabGroup = null;
+  for (let group in lab_codes) {
+    if (lab_codes[group].includes(input)) {
+      foundLabGroup = group;
+      break;
+    }
+  }
+  
+  if (codigos[codigo]) {
+    resultado.textContent = `You're in the group named: ${codigos[codigo]}`;
+    if(foundLabGroup) {
+      labtour.textContent = `You're in the lab tour to ${foundLabGroup}`;
+    } else {
+      labtour.textContent = `No lab tour assigned`;
+    }
+    groupCard.style.display = "block";
+  } else {
+    // resultado.textContent = "Code not found. Please verify that it is correctly written.";
+    alert("Code not found. Please verify that it is correctly written.")
+  }
+}
+
+/*
 function buscarCategoria() {
   const input = document.getElementById("codigo").value.trim().toLowerCase();
   const codigo = input.toUpperCase();
