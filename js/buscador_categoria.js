02a16065ebf6ba13d025b6a6b6d1d777e5e09758
MiguelAlvarezHernanz <124203518+MiguelAlvarezHernanz@users.noreply.github.com>
2025-05-01 17:11:33 +0200

    Update buscador_categoria.js

--- js/buscador_categoria.js
+++ js/buscador_categoria.js
@@ -2,20 +2,27 @@ function buscarCategoria() {
   const input = document.getElementById("codigo").value.trim().toUpperCase();
   
   var groupCard = document.getElementById("group-card");
+  // const room = document.getElementById("room");
   const lab_group = document.getElementById("lab_group");
+  const codigos = document.getElementById("cat_group");
 
-  let foundGroup = null;
-  for (let group in grupos) {
-    if (grupos[group].includes(input)) {
-      foundGroup = group;
+  let foundLabGroup = null;
+  for (let group in lab_codes) {
+    if (lab_codes[group].includes(input)) {
+      foundLabGroup = group;
       break;
     }
   }
 
-  if (foundGroup) {
-    lab_group.textContent = `You're in the lab tour: ${foundGroup}`;
+  if (codigos[input]) {
+    // room.textContent = `You're in the room: ${foundRoom}`;
+    cat_group.textContent = `You're in the gymkhana group: ${codigos[codigo]}`;
     groupCard.style.display = "block";
   } else {
     alert("Code not found. Please verify that it is correctly written.");
+  }
+
+  if (foundLabGroup){
+    lab_group.textContent = `You're in the lab tour: ${foundLabGroup}`;
   }
 }
