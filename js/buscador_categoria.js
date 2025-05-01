a83d3b08745de50d0fc78d64d820dc4e2316ba1f
MiguelAlvarezHernanz <124203518+MiguelAlvarezHernanz@users.noreply.github.com>
2025-05-01 16:19:22 +0200

    Update buscador_categoria.js

--- js/buscador_categoria.js
+++ js/buscador_categoria.js
@@ -2,7 +2,7 @@ function buscarCategoria() {
   const input = document.getElementById("codigo").value.trim().toUpperCase();
   
   var groupCard = document.getElementById("group-card");
-  const resultado = document.getElementById("resultado");
+  const lab_group = document.getElementById("lab_group");
 
   let foundGroup = null;
   for (let group in grupos) {
@@ -13,7 +13,7 @@ function buscarCategoria() {
   }
 
   if (foundGroup) {
-    resultado.textContent = `You're in the lab tour: ${foundGroup}`;
+    lab_group.textContent = `You're in the lab tour: ${foundGroup}`;
     groupCard.style.display = "block";
   } else {
     alert("Code not found. Please verify that it is correctly written.");
