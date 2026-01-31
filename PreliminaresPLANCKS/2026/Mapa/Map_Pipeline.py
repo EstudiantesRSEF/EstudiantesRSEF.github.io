839893b16273d4d0f1cb57c1c721a478b1af4313
Miryam Suárez <mirysuarez2002@gmail.com>
2026-01-31 11:41:41 +0100

    Changes Map

--- PreliminaresPLANCKS/2026/Mapa/Map_Pipeline.py
+++ PreliminaresPLANCKS/2026/Mapa/Map_Pipeline.py
@@ -4,8 +4,8 @@ spreadsheet_id = '16A04FRRVfy10ar0haUjdd1oN_zUZN6BvmZ7gOXxgWVg'
 api_key = 'AIzaSyCs0CmME6XNhtE7j5f_njKqdVl6X0gUWc0'
 sheet_name = "Web"
 
-dict_path = r"/workspaces/EstudiantesRSEF.github.io/PreliminaresPLANCKS/2025/Mapa/university_dic.json"  
-university_data_path = r"/workspaces/EstudiantesRSEF.github.io/PreliminaresPLANCKS/2025/Mapa/universities_data.json"
+dict_path = r"/workspaces/EstudiantesRSEF.github.io/PreliminaresPLANCKS/2026/Mapa/university_dic.json"  
+university_data_path = r"/workspaces/EstudiantesRSEF.github.io/PreliminaresPLANCKS/2026/Mapa/universities_data.json"
 
 sheet_data = get_google_sheet_data(spreadsheet_id,sheet_name, api_key)
 
